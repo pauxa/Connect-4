ca72012e7eb07f726c2f728dbc46d75245e6d155
Elyssa <efsefiane@gmail.com>
2024-11-19 16:22:21 -0600

    Work in progress...

--- connect_four.py
+++ connect_four.py
@@ -1,4 +1,4 @@
-# make the grid
+# 1. Make the grid
 rows, cols = 6, 7
 array = []
 
@@ -7,3 +7,22 @@ for _ in range(rows):
     for _ in range(cols):
         row.append(0)
     array.append(row)
+
+# 2. Get input from the user
+col_num = input("Choose a column.")
+col_num = int(col_num)
+
+# 3. Check if the column is full
+row_num = rows - 1
+
+while array [0][col_num] ! = 0:
+    col_num = input("Please select a new column.")
+    col_num = int(col_num)
+
+# 4. Update the grid
+
+if user1:
+    array[row_num][col_num] = X
+        else = y
+
+# 5. Figure out who won
