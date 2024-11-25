0dc9cd30486f93bba168270c5948a6d011fa9751
Elyssa <efsefiane@gmail.com>
2024-11-25 11:36:29 -0600

    Added Step #4 and modififed Step #5

--- connect_four.py
+++ connect_four.py
@@ -14,15 +14,22 @@ col_num = int(col_num)
 
 # 3. Check if the column is full
 row_num = rows - 1
+user1 = True
 
-while array [0][col_num] ! = 0:
+while array [0][col_num] != 0:
     col_num = input("Please select a new column.")
     col_num = int(col_num)
 
-# 4. Update the grid
+# 4. Find an available row
+
+while array [row_num][col_num] != 0:
+    row_num = row_num - 1
+
+# 5. Update the grid
 
 if user1:
-    array[row_num][col_num] = X
-        else = y
+    array[row_num][col_num] = 'X'
+else:
+    array[row_num][col_num] = 'Y'
 
 # 5. Figure out who won
